e4ff050afc96e3ffe44b05fbe749ed049383d452
AlexPetrusca <petruscalex@gmail.com>
2024-02-11 10:12:00 -0800

    add gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/apetrusca/alpine/alpine-lang
-# Build directory: /Users/apetrusca/alpine/alpine-lang/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TokenizerTests]=] "alp-test")
-set_tests_properties([=[TokenizerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/apetrusca/alpine/alpine-lang/CMakeLists.txt;41;add_test;/Users/apetrusca/alpine/alpine-lang/CMakeLists.txt;0;")
